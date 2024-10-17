fa9b006bfffe2b5b25366506ada48d499a4abead
Paul Arokoola <parokoola@gmail.com>
2024-10-17 13:16:15 +0100

    setup files and icons

---
+++ app/components/icons/ExpandIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ExpandIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.45831 7.29167L9.37498 10L6.45831 12.7083M10.625 7.29167L13.5416 10L10.625 12.7083"
+        stroke="#64748B"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ExpandIcon;
